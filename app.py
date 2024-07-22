4809f7fc16cfa56acbf1145c411cf7c00f4de85d
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-07-22 18:02:02 +0200

    create models for: api_user, device, location

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+from aiohttp import web
 from dotenv import load_dotenv
 from peewee import *
 
@@ -12,3 +13,37 @@ db = PostgresqlDatabase(
     host=os.getenv('DB_HOST'),
     port=int(os.getenv('DB_PORT'))
 )
+
+
+class ApiUser(Model):
+    name = CharField()
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = db
+        table_name = 'api_user'
+
+
+class Location(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+        table_name = 'location'
+
+
+class Device(Model):
+    name = CharField()
+    device_type = CharField()
+    login = CharField()
+    password = CharField()
+    location = ForeignKeyField(Location, backref='devices')
+    api_user = ForeignKeyField(ApiUser, backref='devices')
+
+    class Meta:
+        database = db
+        table_name = 'device'
+
+
+app = web.Application()
